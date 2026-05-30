8c52e4ad9862bf957dbe7c4c14a431e349c93018
Menghor <menghorzz6@gmail.com>
2026-05-30 10:21:25 +0700

    Describe your update

--- src/components/Products/ProductDetail.js
+++ src/components/Products/ProductDetail.js
@@ -21,7 +21,7 @@ const ProductDetail = () => {
   const [sizesArray, setSizesArray] = useState([]);
   const [sizeStockMap, setSizeStockMap] = useState({});
 
-  const BASE_URL = 'http://127.0.0.1:8000';
+  const BASE_URL = 'https://backend-ecommerce-6hef.onrender.com';
 
   useEffect(() => {
     if (id) {
